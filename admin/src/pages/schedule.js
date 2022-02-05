0428e4633f44968a5fb3a4fdb3e49f2b3c2755e3
BarokDG <it.barok.dagim@gmail.com>
2022-02-06 01:02:11 +0300

    schedule preview area added

--- admin/src/pages/schedule.js
+++ admin/src/pages/schedule.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import axios from "../services/axios";
 
 export default function Schedule() {
+  const [preview, setPreview] = useState([
+    "Schedule",
+    "Team 1",
+    "Team 2",
+    "Time",
+  ]);
+
   const addFixture = function (e) {
     e.preventDefault();
 
@@ -21,27 +29,45 @@ export default function Schedule() {
     <form onSubmit={addFixture} method="post">
       <div className="form-group">
         <h3>Select teams</h3>
-        <select name="team1" id="">
-          <option value="team1">Team 1</option>
-          <option value="team2">Team 2</option>
-          <option value="team3">Team 3</option>
-          <option value="team4">Team 4</option>
-          <option value="team5">Team 5</option>
-          <option value="team6">Team 6</option>
-          <option value="team7">Team 7</option>
-          <option value="team8">Team 8</option>
-          <option value="team9">Team 9</option>
+        <select
+          name="team1"
+          id=""
+          onChange={(e) => {
+            preview.splice(1, 1, e.target.value);
+            setPreview([...preview]);
+          }}
+        >
+          <option value="">--Select team 1--</option>
+          <option value="IT">IT</option>
+          <option value="Chemical">Chemical</option>
+          <option value="Electrical C">Electrical C</option>
+          <option value="Biomed">Biomed</option>
+          <option value="Software">Software</option>
+          <option value="Mechanical">Mechanical</option>
+          <option value="Civil">Civil</option>
+          <option value="Electrical B1">Electrical B1</option>
+          <option value="Electrical B2">Electrical B2</option>
+          <option value="Electrical A">Electrical A</option>
         </select>
-        <select name="team2" id="">
-          <option value="team1">Team 1</option>
-          <option value="team2">Team 2</option>
-          <option value="team3">Team 3</option>
-          <option value="team4">Team 4</option>
-          <option value="team5">Team 5</option>
-          <option value="team6">Team 6</option>
-          <option value="team7">Team 7</option>
-          <option value="team8">Team 8</option>
-          <option value="team9">Team 9</option>
+        <select
+          name="team2"
+          id=""
+          onChange={(e) => {
+            preview.splice(2, 1, e.target.value);
+            setPreview([...preview]);
+          }}
+        >
+          <option value="">--Select team 2--</option>
+          <option value="IT">IT</option>
+          <option value="Chemical">Chemical</option>
+          <option value="Electrical C">Electrical C</option>
+          <option value="Biomed">Biomed</option>
+          <option value="Software">Software</option>
+          <option value="Mechanical">Mechanical</option>
+          <option value="Civil">Civil</option>
+          <option value="Electrical B1">Electrical B1</option>
+          <option value="Electrical B2">Electrical B2</option>
+          <option value="Electrical A">Electrical A</option>
         </select>
       </div>
 
@@ -63,6 +89,23 @@ export default function Schedule() {
           <option value="8">Gameweek 8</option>
           <option value="9">Gameweek 9</option>
         </select>
+      </div>
+
+      <div className="preview">
+        <h3>Preview</h3>
+        <div className="box">
+          {/* <div className="switch">
+            <select name="" id="">
+              <option value="">Gameweek 1</option>
+            </select>
+          </div> */}
+          <p className="schedule">{preview[0]}</p>
+          <div className="scoreline">
+            <div className="team1">{preview[1]}</div>
+            <p className="time">{preview[3]}</p>
+            <div className="team2">{preview[2]}</div>
+          </div>
+        </div>
       </div>
 
       <div className="form-group">
