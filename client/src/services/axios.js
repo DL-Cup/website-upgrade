92fbe58fab8f2a0001c2108991d76cb2891b2f49
BarokDG <it.barok.dagim@gmail.com>
2022-12-03 13:02:47 +0300

    update dependencies, update api url

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://dlcup.herokuapp.com";
+const baseUrl = "https://dlcup-server.onrender.com/";
 const instance = axios.create({
   baseURL: baseUrl,
   headers: {
