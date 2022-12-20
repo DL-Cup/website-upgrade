fdf9e55e062ffa9efe5f72f523e9c015373bf342
BarokDG <it.barok.dagim@gmail.com>
2022-12-20 13:29:14 +0300

    Remove selection context, remove unnecessary components

--- client/src/components/stats.js
+++ client/src/components/stats.js
@@ -1,102 +1,15 @@
-import { useState, useEffect, useContext } from "react";
-import { getTable, getPlayers, getFixtures } from "./services/services";
+import { useState, useEffect } from "react";
+import { getTable, getPlayers } from "./services/services";
 import "./css/stats.css";
 
-import { SelectionContext } from "./services/selectionContext";
-
 import { ReactComponent as StrikerIcon } from "./images/striker.svg";
-// import { ReactComponent as WallIcon } from "./images/wall.svg";
 
 function Stats() {
   return (
-    <div className="desktop-stats">
-      <div className="team-info">
-        <TeamFixturesAndResults />
-      </div>
-      <div className="league-stats">
-        <TopScorer />
-        <MostCleansheets />
-      </div>
+    <div className="league-stats">
+      <TopScorer />
+      <MostCleansheets />
     </div>
-  );
-}
-
-function TeamFixturesAndResults() {
-  // make leading team selected team by default
-  const [TeamResults, setTeamResults] = useState();
-  const [TableInfo, setTableInfo] = useState();
-  const { selectedTeam } = useContext(SelectionContext);
-
-  useEffect(() => {
-    getFixtures(selectedTeam).then((res) => setTeamResults(res));
-  }, [selectedTeam]);
-
-  useEffect(() => {
-    getTable().then((res) => setTableInfo(res));
-  }, []);
-
-  const leaguePositions = {};
-
-  TableInfo?.forEach(({ teamName }, index) => {
-    leaguePositions[teamName] = index + 1;
-  });
-
-  let TeamsPlayed = [];
-
-  TeamResults?.forEach(({ teams }) => {
-    let [team1, team2] = teams;
-    team1 === selectedTeam ? TeamsPlayed.push(team2) : TeamsPlayed.push(team1);
-  });
-
-  return (
-    <>
-      <div className="recent-results">
-        <h4>Recent results</h4>
-        <hr />
-        <>
-          {TeamResults?.slice(0, 5).map(({ teams, score, matchID }) => {
-            let [, , team1, team2] = teams;
-
-            return (
-              <div className="match" key={matchID}>
-                <span>{team1}</span>
-                <span className="score">{score}</span>
-                <span>{team2}</span>
-              </div>
-            );
-          })}
-        </>
-      </div>
-      <div className="remaining-fixtures">
-        <h4>Remaining fixtures</h4>
-        <hr />
-        <table>
-          <colgroup>
-            <col />
-            <col className="leaguePos" />
-          </colgroup>
-          <thead>
-            <tr>
-              <th></th>
-              <th>League Position</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(leaguePositions)?.map((team) => {
-              if (TeamsPlayed.includes(team) || team === selectedTeam) {
-                return null;
-              } else
-                return (
-                  <tr>
-                    <td>{team}</td>
-                    <td>{leaguePositions[team]}</td>
-                  </tr>
-                );
-            })}
-          </tbody>
-        </table>
-      </div>
-    </>
   );
 }
 
--- client/src/pages/tables.js
+++ client/src/pages/tables.js
@@ -1,18 +1,5 @@
 import Table from "../components/leagueTable";
-// import Stats from "./../stats";
-
-import { SelectionContext } from "../services/selectionContext";
-import { useState } from "react";
 
 export default function Tables() {
-  const [selectedTeam, setSelectedTeam] = useState();
-
-  return (
-    <div className="grid-container">
-      <SelectionContext.Provider value={{ selectedTeam, setSelectedTeam }}>
-        <Table />
-        {/* <Stats /> */}
-      </SelectionContext.Provider>
-    </div>
-  );
+  return <Table />;
 }
