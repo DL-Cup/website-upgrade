401ab14db022e762fdbb61cb126d1232490296a8
BarokDG <it.barok.dagim@gmail.com>
2022-12-24 12:20:32 +0300

    Basic desktop view

--- client/src/views/desktopView.js
+++ client/src/views/desktopView.js
@@ -1,13 +1,18 @@
 import LeagueTable from "../components/leagueTable";
 import TopScorers from "../components/topScorers";
+import Fixtures from "../pages/fixtures";
 import MostCleansheets from "../components/mostCleansheets";
 import Header from "../components/header";
 
 function DesktopView() {
   return (
     <>
+      {/* <Header /> */}
       <main className="desktop-view">
         <LeagueTable />
+        <section>
+          <Fixtures />
+        </section>
         <aside>
           <TopScorers />
           <MostCleansheets />
